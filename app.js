e4b60ff340808155afce05ab8e394a920d3a5624
LaShenaButler <joinadidasradio@gmail.com>
2024-07-23 12:00:42 -0400

    just fixed my clear and equal buttons

--- app.js
+++ app.js
@@ -10,11 +10,16 @@
 
     function solve() {
         // grab the expression/value in the input box and add it to variable x
-        var x = document.getElementById('output').value;
-        
+        var x = document.getElementById('output').value;       
 
         // solve the expression and assign it variable y 
-        // var y = eval(x); eval() function should be avoided for security reasons in web applications.
+        var y = eval(x); //eval() function should be avoided for security reasons in web applications.
         // display y in the input/output box
         document.getElementById('output').value = y;
     }
+
+    // add an event listener to the clear button
+    document.getElementById('btnClear').addEventListener('click', clearScreen);
+
+    // add an event listener to the solve button
+    document.getElementById('btnSolve').addEventListener('click', solve);

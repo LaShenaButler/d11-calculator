31a47cd1cbaec7c8aca6c180aec4406355341d51
LaShenaButler <joinadidasradio@gmail.com>
2024-07-23 11:50:39 -0400

    making modifications to break and fix codes testing

--- app.js
+++ app.js
@@ -10,16 +10,11 @@
 
     function solve() {
         // grab the expression/value in the input box and add it to variable x
-
         var x = document.getElementById('output').value;
         
 
         // solve the expression and assign it variable y 
-
-       // var y = eval(x); eval() function should be avoided for security reasons in web applications.
-
-       var y = eval(x);
-
+        // var y = eval(x); eval() function should be avoided for security reasons in web applications.
         // display y in the input/output box
         document.getElementById('output').value = y;
     }
